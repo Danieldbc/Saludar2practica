36f536338f89d9699c9113d51bf0a35004d612ff
Danieldbc <daniel.dbc01@gmail.com>
2017-10-16 20:31:47 +0200

    Versión final

--- app/src/main/java/edu/eseiaat/upc/pma/borreguero/daniel/saludar2practica/PrincipalActivity.java
+++ app/src/main/java/edu/eseiaat/upc/pma/borreguero/daniel/saludar2practica/PrincipalActivity.java
@@ -1,5 +1,6 @@
 package edu.eseiaat.upc.pma.borreguero.daniel.saludar2practica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,17 +8,34 @@ import android.widget.TextView;
 
 public class PrincipalActivity extends AppCompatActivity {
     private TextView tvPrincipal;
+    private String actual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
         tvPrincipal=(TextView)findViewById(R.id.etHolaMon);
-        String actual=tvPrincipal.getText().toString();
-        String inicial="mon";
+        actual=tvPrincipal.getText().toString();
+        String inicial=getString(R.string.mundo);
         tvPrincipal.setText(String.format(actual,inicial));
+
     }
 
     public void Principal(View view) {
+        Intent intent=new Intent(this,SecundariaActivity.class);
+        startActivityForResult(intent,SecundariaActivity.pasar);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==SecundariaActivity.pasar){
+            if (resultCode==RESULT_OK){
+                String cambiar=data.getExtras().getString(SecundariaActivity.keyname);
+                tvPrincipal.setText(String.format(actual,cambiar));
+            }
+        }
+
     }
 }
--- app/src/main/java/edu/eseiaat/upc/pma/borreguero/daniel/saludar2practica/SecundariaActivity.java
+++ app/src/main/java/edu/eseiaat/upc/pma/borreguero/daniel/saludar2practica/SecundariaActivity.java
@@ -1,17 +1,29 @@
 package edu.eseiaat.upc.pma.borreguero.daniel.saludar2practica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SecundariaActivity extends AppCompatActivity {
+    private EditText etSecundaria;
+    public static int pasar=1;
+    public static String keyname="keyname";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secundaria);
+
+        etSecundaria=(EditText)findViewById(R.id.etSecundario);
     }
 
     public void Secundario(View view) {
+        String nom=etSecundaria.getText().toString();
+        Intent data=new Intent();
+        data.putExtra(keyname,nom);
+        setResult(RESULT_OK,data);
+        finish();
     }
 }
